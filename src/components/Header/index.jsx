28ba9bb7fafc50850de7f69ae062e2e62e1df562
Alisson Renan <renalima.rl46@gmail.com>
2022-11-05 22:46:30 -0300

    Adding header stylesheets

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,20 @@
+import Logo from "../../svg/logo.svg"
+
 export default function Header(){
+    const descLogo = "Essa é a logo do blog"
     return (
-        <header>
-            <h1>Header</h1>
+        <header className="flex-space-between">
+            <div className="header-logo">
+                <img src={Logo} alt={descLogo} />
+            </div>
+            <div className="header-search">
+                <input type="text" className="input-search" name="search" id="" placeholder="Buscar artigos" />
+            </div>
+            <ul className="header-menu">
+                <li><a href="#" className="nav-link">Categories</a></li>
+                <li><a href="#" className="nav-link">About</a></li>
+                <li><a href="#" className="nav-link">Contact</a></li>
+            </ul>
         </header>
     )
 }
